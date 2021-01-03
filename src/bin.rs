3ec98a7b0f1d2794d2753e568518c249739c9663
Mariot Tsitoara <mariot.tsitoara@gmail.com>
2021-01-03 22:14:12 +0100

    Add reload and sleep actions

--- src/bin.rs
+++ src/bin.rs
@@ -5,10 +5,12 @@ extern crate log;
 
 use std::env;
 use std::fs::create_dir_all;
+use std::path::PathBuf;
+use std::time::{Duration, Instant};
+use std::thread;
 
 use clap::App;
 use indicatif::{ProgressBar, ProgressStyle};
-use reqwest::Error;
 use reqwest::blocking::Client;
 
 use chan_downloader::{get_image_links, get_page_content, get_thread_infos, save_image};
@@ -20,30 +22,36 @@ fn main() {
 
     let thread = matches.value_of("thread").unwrap();
     let output = matches.value_of("output").unwrap_or("downloads");
+    let reload: bool = matches.is_present("reload");
+    let interval: u64 = matches.value_of("interval").unwrap_or("5").parse().unwrap();
+    let limit: u64 = matches.value_of("limit").unwrap_or("120").parse().unwrap();
+
+    let mut execution_number: u64 = 0;
+    let number_of_execution: u64 = if reload {limit / interval} else { 1 };
+
     info!(target: "downloader_events", "Downloading images from {} to {}", thread, output);
-    download_thread(thread, &output).unwrap();
+
+    let directory = create_directory(thread, &output);
+
+    let wait_time = Duration::from_millis(60000 * interval);
+    while execution_number != number_of_execution {
+        let start = Instant::now();
+        execution_number += 1;
+        explore_thread(thread, &directory);
+        let runtime = start.elapsed();
+        if let Some(remaining) = wait_time.checked_sub(runtime) {
+            info!(
+                "Schedule slice has time left over; sleeping for {:?}",
+                remaining
+            );
+            thread::sleep(remaining);
+        }
+        info!(target: "downloader_events", "Downloader executed {} time{}", execution_number, if execution_number == 1 { "" } else {"s"} );
+    }
 }
 
-fn download_thread(thread_link: &str, output: &str) -> Result<String, Error> {
-    let client = Client::builder().user_agent("reqwest").build()?;
-    let workpath = env::current_dir().unwrap();
-    info!("Working from {}", workpath.display());
-
-    let (board_name, thread_id) = get_thread_infos(thread_link);
-
-    let directory = workpath.join(output).join(board_name).join(thread_id);
-    if !directory.exists() {
-        match create_dir_all(&directory) {
-            Ok(_) => {
-                info!("Created directory {}", directory.display());
-            }
-            Err(err) => {
-                error!("Failed to create new directory: {}", err);
-                eprintln!("Failed to create new directory: {}", err);
-            },
-        }
-    }
-
+fn explore_thread(thread_link: &str, directory: &PathBuf) {
+    let client = Client::builder().user_agent("reqwest").build().unwrap();
     let page_string = match get_page_content(thread_link, &client) {
         Ok(page_string) => {
             info!("Loaded content from {}", thread_link);
@@ -87,7 +95,27 @@ fn download_thread(thread_link: &str, output: &str) -> Result<String, Error> {
         pb.inc(1);
     }
     pb.finish_with_message("Done");
-    info!("Download finished");
+}
 
-    Ok(format!("Downloaded: {} in {}", thread_link, output))
+fn create_directory(thread_link: &str, output: &str) -> PathBuf {
+    let workpath = env::current_dir().unwrap();
+    info!("Working from {}", workpath.display());
+
+    let (board_name, thread_id) = get_thread_infos(thread_link);
+
+    let directory = workpath.join(output).join(board_name).join(thread_id);
+    if !directory.exists() {
+        match create_dir_all(&directory) {
+            Ok(_) => {
+                info!("Created directory {}", directory.display());
+            }
+            Err(err) => {
+                error!("Failed to create new directory: {}", err);
+                eprintln!("Failed to create new directory: {}", err);
+            },
+        }
+    }
+    
+    info!("Downloaded: {} in {}", thread_link, output);
+    directory
 }
